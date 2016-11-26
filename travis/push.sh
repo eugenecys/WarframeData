02317a1c5e85ba1a3594aff2a98b396994cc8e4e
BuildTools <jakeincanada@icloud.com>
2016-11-25 21:12:51 -0500

    Create travis/push.sh

---
+++ travis/push.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+setup_git() {
+  git config --global user.name "macdja38"
+  git config --global push.default simple
+}
+
+commit_website_files() {
+  git add YAML/*.yaml
+  git add JSON/*.json
+  git commit --message "Travis build: $TRAVIS_BUILD_NUMBER"
+}
+
+upload_files() {
+  git remote remove origin
+  git remote add origin https://${GH_TOKEN}@github.com/macdja38/WarframeData.git > /dev/null 2>&1
+  git push origin HEAD:master
+}
+
+setup_git
+commit_website_files
+upload_files
